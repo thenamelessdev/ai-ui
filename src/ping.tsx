b628d643deda7700582477977770a0fd12b48a82
thenamelessdev <aron.minarovits@gmail.com>
2026-01-03 19:51:53 +0100

    add useeffect hook to ping so it doesnt sends the request so may times

--- src/ping.tsx
+++ src/ping.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 
 export default function Ping(){
     const [ stat, editStat ] = useState("loading...");
+    const [ version, setVersion ] = useState("loading...");
 
     function getUrl(){
         return Cookies.get("url") || "http://localhost:11434";
@@ -21,16 +22,33 @@ export default function Ping(){
         catch{
             editStat("error")
         }
+        try{
+            const response = await fetch(`${getUrl()}/api/version`);
+            const json = await response.json();
+            if(response.ok){
+                setVersion(json.version);
+            }
+            else{
+                setVersion("error");
+            }
+        }
+        catch{
+            setVersion("error");
+        }
     }
-    ping()
 
-    setInterval(() => {
+    useEffect(() => {
         ping();
-    }, 5000);
+
+        setInterval(() => {
+            ping();
+        }, 5000);
+    }, [])
 
     return(
         <div>
             <p>Status: {stat}</p>
+            <p>Version: {version}</p>
         </div>
     );
 }
