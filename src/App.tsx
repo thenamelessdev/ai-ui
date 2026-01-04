181333e9b299549da51ce6a6a6448b366bbbe17d
thenamelessdev <aron.minarovits@gmail.com>
2026-01-04 15:59:38 +0100

    refactor: reorganize component structure by moving UI elements to Headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import Headers from "./headers"
-import Ping from "./ping";
-import Ask from "./Ask";
-import Url from "./Url";
-import Models from "./Models";
 
 function App() {
   return(
     <>
       <Headers/>
-      <Ping/>
-      <Ask/>
-      <Url/>
-      <Models/>
     </>
   );
 }
--- src/headers.tsx
+++ src/headers.tsx
@@ -1,9 +1,30 @@
+import Ping from "./ping";
+import Ask from "./Ask";
+import Url from "./Url";
+import Models from "./Models";
+import { useState } from "react";
+
 export default function Headers(){
+
+    const [page, setPage] = useState("");
+
+
     return(
         <div>
             <h1>AI UI</h1>
             <p><b>A UI for Ollama</b></p>
+            <br />
+            <div className="btn-group">
+                <button className="btn btn-primary" onClick={() => setPage("ask")}>Ask</button>
+                <button className="btn btn-primary" onClick={() => setPage("url")}>Edit URL</button>
+                <button className="btn btn-primary" onClick={() => setPage("models")}>Models</button>
+            </div>
             <hr />
+            <Ping/>
+            <hr />
+            {page == "ask" && <Ask/>}
+            {page == "url" && <Url/>}
+            {page == "models" && <Models/>}
         </div>
     )
 }
