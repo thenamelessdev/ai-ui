5cdcc72351dc31a3d6ef5c5936f7d09d9dfb1ac2
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2026-01-11 12:21:17 +0100

    made logo, new navbar thing

--- src/headers.tsx
+++ src/headers.tsx
@@ -11,14 +11,31 @@ export default function Headers(){
 
     return(
         <div>
-            <h1>AI UI</h1>
-            <p><b>A UI for Ollama</b></p>
-            <br />
-            <div className="btn-group">
-                <button className="btn btn-primary" onClick={() => setPage("ask")}>Ask</button>
-                <button className="btn btn-primary" onClick={() => setPage("url")}>Edit URL</button>
-                <button className="btn btn-primary" onClick={() => setPage("models")}>Models</button>
+
+            <nav className="navbar bg-body-tertiary navbar-expand-lg">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                <img src="Public/ai ui logo.svg" alt="Logo" width="40" height="40" className="d-inline-block align-text-top" />
+                AI UI
+                </a>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" onClick={() => setPage("ask")} style={{cursor: "pointer"}}>Ask</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" onClick={() => setPage("url")} style={{cursor: "pointer"}}>Edit URL</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" onClick={() => setPage("models")} style={{cursor: "pointer"}}>Models</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
+            </nav>
+
             <hr />
             <Ping/>
             <hr />
